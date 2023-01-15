eb6de559eff828dbe585208807a41aeb7ec50319
veyseltosun <postalarim1923@gmail.com>
2023-01-15 20:20:45 +0300

    ..

--- src/index.js
+++ src/index.js
@@ -1,18 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-const App = () => {
-     
-    window.navigator.geolocation.getCurrentPosition(
-        (position) => console.log(position),
-        (error) => console.log(error)
-    )
 
-    return(
-        <div>Hello World!</div>
-    )
+class App extends React.Component{
+    render(){
+        window.navigator.geolocation.getCurrentPosition(
+            (position) => console.log(position),
+            (error) => console.log(error)
+        )
+      return(
+        <div>
+            You are in the northern hemisphere...
+        </div>
+      )  
+    }
 }
-
 ReactDOM.render(
     <App/>,
     document.querySelector("#root")
