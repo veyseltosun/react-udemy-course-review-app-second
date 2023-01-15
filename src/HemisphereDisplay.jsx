6e92d89021c802131e7511c64e45567712dbc09e
veyseltosun <postalarim1923@gmail.com>
2023-01-16 02:25:58 +0300

    ...

--- src/HemisphereDisplay.jsx
+++ src/HemisphereDisplay.jsx
@@ -1,11 +1,37 @@
 import React from "react";
+import northernPic from "./images/NorthernHemisphere.jpg";
+import southernPic from "./images/SouthernHemisphere.jpg";
+
+const hemisphereConfig = {
+    Northern : {
+        text: "it is northern hemisphere",
+        picture: northernPic,
+    },
+    Southern: {
+        text: "it is southern hemisphere",
+        picture: southernPic,
+    }
+}
+
+const HemisphereDisplay = ({ latitude }) => {
+    const hemisphere = latitude > 0 ? "Northern" : "Southern";
+    const {text, picture} = hemisphereConfig[hemisphere];
+    
 
 
-const HemisphereDisplay = ( props ) => {
-    console.log(props.latitude)
-    return(
+    return (
         <div>
-            Hey you are in the Northern hemisphere !
+            <div>
+
+                {text}
+
+            </div>
+            <div>
+                
+                <img src={picture} alt="globe" />
+
+            </div>
+
         </div>
     )
 }
